60d3c742664884f1734ef220a102e66c9d0110b2
ecmascriptguru <ecmascript.guru@gmail.com>
2017-03-31 19:59:59 -0400

    login api created

--- app/Http/Controllers/Auth/APIController.php
+++ app/Http/Controllers/Auth/APIController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth;
+use Illuminate\Support\Facades\Auth;
 use App\User;
 
 class APIController extends Controller
 {
-    public function login()
-    {
-        if (Auth::attempt(['email' => $email, 'password' => $password])) {
-            // Authentication passed...
-            // return redirect()->intended('dashboard');
-            return Response::json([
-            	'status' => true
-            ]);
-        }
-    }
+	public function login(Request $request)
+	{
+		$email = $request->input('email');
+		$password = $request->input('password');
+		if (Auth::attempt(['email' => $email, 'password' => $password])) {
+			echo json_encode(array(
+				'status' => true,
+				'user' => Auth::user()
+			));
+		} else {
+			echo json_encode(array(
+				'status' => false,
+				'user' => null
+			));
+		}
+	}
 }
